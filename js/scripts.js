2d2105f84bd039d715f3c2122110883cd5188cdc
cord walsh and Sam Cook <cord@>
2019-06-03 11:51:55 -0700

    add jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+$(function() {
 var add = function(number1, number2) {
   return number1 + number2;
 };
@@ -6,3 +7,5 @@ var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
 var result = add(number1, number2);
 alert(result);
+
+});
